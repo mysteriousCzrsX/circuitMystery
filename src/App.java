20f38ba281ee3ce748d0b94e876095c1be7ded0c
Cezary Wieczorkowski <cezary.wieczorkowski@3mdeb.com>
2024-12-05 15:46:43 +0100

    Test

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import CircuitMystery.MysteryWindow;
 /**
  * Main app class that creates game window and starts the game loop.
  */
+
 public class App {
     public static void main(String[] args) throws Exception {
         MysteryWindow window = new MysteryWindow(800, 600);
